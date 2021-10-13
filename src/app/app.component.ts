d0db784aadd6f59d1ec8539632e391125ab871b4
GustavoAntonelli <gustavo.antonelli.souza@hotmail.com>
2021-10-13 17:52:52 -0300

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'aula';
+  name:string ="";
+  address:string ="";
+  phone:string ="";
+  age:number = 0;
+
+
+  clients:any = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  save() {
+      this.clients.push(
+        {
+        name: this.name,
+        phone: this.phone,
+        age: this.age
+      }
+    );
+    this.cancel()
+    console.log(this.clients);
+  }
+  
+  cancel() {
+    this.name = "";
+    this.address = "";
+    this.phone = "";
+    this.age = 0;
+  }
+
+  delete(i:number){
+    this.clients.splice(i, 1)
+  }
 }
